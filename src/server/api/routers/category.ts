2ebd633d60b5f2b5ca39ba14c0bd82106ad83540
ferzian <m.ferzian09@gmail.com>
2025-05-28 21:34:57 +0700

    first commit

---
+++ src/server/api/routers/category.ts
@@ -0,0 +1,12 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const categoryRouter = createTRPCRouter({
+    getCategories: protectedProcedure.query(async ({ctx}) => {
+        const { db } = ctx;
+
+        const categories = await db.category.findMany();
+
+        return categories;
+        
+    }),
+});
